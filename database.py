042abbf4d9c55f45b489331beaca9e38ae7697b3
CATCUSSSSSSS <iliahoshmand8@gmail.com>
2026-08-06 01:03:30 +0330

    Create database.py

---
+++ database.py
@@ -0,0 +1,77 @@
+import aiosqlite
+
+DB_NAME = "catcus_bot.db"
+
+
+async def init_db():
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS users (
+            user_id INTEGER PRIMARY KEY,
+            username TEXT,
+            full_name TEXT,
+            mode TEXT DEFAULT 'anonymous',
+            banned INTEGER DEFAULT 0
+        )
+        """)
+
+        await db.execute("""
+        CREATE TABLE IF NOT EXISTS messages (
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            user_id INTEGER,
+            message_id INTEGER,
+            mode TEXT
+        )
+        """)
+
+        await db.commit()
+
+
+async def add_user(user_id, username, full_name):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute(
+            """
+            INSERT OR IGNORE INTO users(user_id, username, full_name)
+            VALUES (?, ?, ?)
+            """,
+            (user_id, username, full_name)
+        )
+        await db.commit()
+
+
+async def get_user_mode(user_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        cursor = await db.execute(
+            "SELECT mode FROM users WHERE user_id=?",
+            (user_id,)
+        )
+        result = await cursor.fetchone()
+        return result[0] if result else "anonymous"
+
+
+async def change_mode(user_id, mode):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute(
+            "UPDATE users SET mode=? WHERE user_id=?",
+            (mode, user_id)
+        )
+        await db.commit()
+
+
+async def ban_user(user_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        await db.execute(
+            "UPDATE users SET banned=1 WHERE user_id=?",
+            (user_id,)
+        )
+        await db.commit()
+
+
+async def is_banned(user_id):
+    async with aiosqlite.connect(DB_NAME) as db:
+        cursor = await db.execute(
+            "SELECT banned FROM users WHERE user_id=?",
+            (user_id,)
+        )
+        result = await cursor.fetchone()
+        return bool(result and result[0] == 1)
